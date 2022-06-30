2e5596f1c404e08f3f83f39f9edfb5036876b5a7
elpericoo <elpericoo@gmail.com>
2022-06-30 10:47:25 +0200

    feat: add list activity

--- app/src/main/java/com/elpericoo/listas/LoginFormFragment.kt
+++ app/src/main/java/com/elpericoo/listas/LoginFormFragment.kt
@@ -1,9 +1,12 @@
 package com.elpericoo.listas
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.jocnunez.listas.R
 
@@ -14,6 +17,18 @@ class LoginFormFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login_form, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_login_form, container, false)
+
+        val nextButton = view.findViewById<Button>(R.id.nextButton)
+        nextButton.setOnClickListener {
+            Log.d("debug", "Click on next button")
+        val intent = Intent(context, ListActivity::class.java).apply {  }
+            Log.d("debug", "New List")
+            startActivity(intent)
+
+        }
+        return view
+
+
+        }
 }
